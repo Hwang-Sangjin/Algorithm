a2a0ce154334c29803b10a7be30b0af493b72076
Hwang-Sangjin <aosdb5640@naver.com>
2021-07-02 17:41:37 +0900

    15654 N과M(5)

---
+++ BOJ/N과M/15654.cpp
@@ -0,0 +1,50 @@
+#include <string>
+#include <vector>
+#include <queue>
+#include <iostream>
+#include <cstring>
+#include <cstdio>
+#include <algorithm>
+#include <math.h>
+
+using namespace std;
+int N, M;
+bool check[10];
+vector<int> arr;
+vector<int>v;
+
+void recursive(int x) {
+    if (x == M) {
+        for (auto t : v) {
+            cout << t << ' ';
+        }
+        cout << '\n';
+        return;
+    }
+
+    for (int i = 0; i < N; i++) {
+        if (check[i]) continue;
+        check[i] = true;
+        v.push_back(arr[i]);
+        recursive(x + 1);
+        v.pop_back();
+        check[i] = false;
+    }
+}
+
+int main() {
+    
+    
+    cin >> N >> M;
+
+    int temp;
+    for (int i = 0; i < N; i++) {
+        cin >> temp;
+        arr.push_back(temp);
+    }
+    sort(arr.begin(), arr.end());
+
+    recursive(0);
+    
+    return 0;
+}
