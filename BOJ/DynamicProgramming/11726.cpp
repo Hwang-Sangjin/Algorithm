55798b9fa6c8602f3b0729f3aad1c1d9f59d7ece
Hwang-Sangjin <aosdb5640@naver.com>
2021-07-12 21:06:19 +0900

    11726 2xN타일링

---
+++ BOJ/DynamicProgramming/11726.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int dp[1002];
+
+int main() {
+
+	int n;
+	cin >> n;
+
+	dp[1] = 1;
+	dp[2] = 2;
+
+
+	for (int i = 3; i <= n; i++) {
+		dp[i] = (dp[i - 1] + dp[i - 2]) % 10007;
+	}
+	cout << dp[n];
+
+	return 0;
+}
