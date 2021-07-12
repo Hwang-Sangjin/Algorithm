0718c1c56259881ee0d94b4a8e373bbdfe64927e
Hwang-Sangjin <aosdb5640@naver.com>
2021-07-12 21:56:54 +0900

    11052 카드 구매하기

---
+++ BOJ/DynamicProgramming/11052.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int dp[1002];
+
+int main() {
+
+	int n;
+	cin >> n;
+
+	vector<int> v;
+	int temp;
+	v.push_back(0);
+	for (int i = 0; i < n; i++) {
+		cin >> temp;
+		v.push_back(temp);
+	}
+
+	dp[1] = v[1];
+
+	for (int i = 2; i <= n; i++) {
+		dp[i] = v[i];
+		for (int k = 1; k < i; k++) {
+			dp[i] = max(dp[i], dp[i - k] + v[k]);
+		}
+	}
+	cout << dp[n];
+	return 0;
+}
