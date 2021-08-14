c5c1f862a307104e3b9d7ffc061eeb39e097724a
Hwang-Sangjin <52124805+Hwang-Sangjin@users.noreply.github.com>
2021-08-15 00:23:45 +0900

    점프와 순간 이동

---
+++ Programmers/if/jump&tele.js
@@ -0,0 +1,15 @@
+function solution(n)
+{
+    var ans = 0;
+    while(n !==0){
+        if(n%2 === 0){
+           n = n/2;
+        }
+        else{
+            n--;
+            ans++;
+        }
+    }
+
+    return ans;
+}
