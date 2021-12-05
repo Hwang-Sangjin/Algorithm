ea3ce7b9386898d4721d756d70b5d087cc79da7b
Hwang-Sangjin <aosdb5640@naver.com>
2021-12-05 16:10:15 +0900

    12865 평범한 배낭

---
+++ BOJ/0/1knapsack/12865.cpp
@@ -0,0 +1,44 @@
+#include "pch.h"
+#include <iostream>
+#include <algorithm>
+#include <stdlib.h>
+#include <vector>
+#include <queue>
+#include <stack>
+#include <deque>
+#include <cstring>
+#include <string>
+#include <cmath>
+#include <cstdio>
+#include <stdio.h>
+
+int di[4] = { -1,0,1,0 };
+int dj[4] = { 0,1,0,-1 };
+
+using namespace std;
+int dp[101][100001];
+int w[101];     // 물건의 무게
+int v[101];
+
+int main() {
+	int N, K,ans=0;
+	cin >> N >> K;
+
+	for (int i = 1; i <= N; i++) {
+		cin >> w[i] >> v[i];
+	}
+
+	for (int i = 1; i <= N; i++) {
+		for (int j = 1; j <= K; j++) {
+			if (j < w[i]) {
+				dp[i][j] = dp[i - 1][j];
+			}
+			else {
+				dp[i][j] = max(dp[i - 1][j], dp[i - 1][j - w[i]] + v[i]);
+			}
+		}
+	}
+	cout << dp[N][K];
+
+	return 0;
+}
