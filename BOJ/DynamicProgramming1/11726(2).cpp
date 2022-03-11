87f16cbe9416ded4962af03fce1dfc3c9cf15367
Hwang-Sangjin <52124805+Hwang-Sangjin@users.noreply.github.com>
2022-03-11 21:43:19 +0900

    11726 2xN 타일링

---
+++ BOJ/DynamicProgramming1/11726(2).cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <algorithm>
+#include <stdlib.h>
+#include <vector>
+#include <queue>
+#include <stack>
+#include <deque>
+#include <cstring>
+#include <string> 
+#include <cmath>
+#include <cstdio>
+#include <set>
+#include <map>
+
+using namespace std;
+int dp[1002];
+
+int main() {
+	int n;
+	cin >> n;
+
+	dp[1] = 1;
+	dp[2] = 2;
+
+	for (int i = 3; i <= n; i++) {
+		dp[i] = (dp[i - 1] + dp[i - 2]) % 10007;
+	}
+	cout << dp[n];
+}
